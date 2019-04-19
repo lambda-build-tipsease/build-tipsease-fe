1c63b9a07056c2e41365b29a86c64221599cf612
Shalom <shalom@Shaloms-MBP.home>
2019-04-19 11:49:58 -0400

    add semantic-ui to package.json, fix tipping

--- tipsease/src/components/container/WorkerProfile.js
+++ tipsease/src/components/container/WorkerProfile.js
@@ -1,26 +1,28 @@
-import React from 'react'
-import WorkerList from './WorkerList';
+import React from "react";
+import WorkerList from "./WorkerList";
 import SuccessModal from "./SuccessModal";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 import { getWorkerProfile, sendTip } from "../../actions";
 import { Modal } from "semantic-ui-react";
-import NavMenu from './NavMenu'
+import NavMenu from "./NavMenu";
 
 const mapStateToProps = state => ({
-    serviceWorker: state.serviceWorker,
-    state
-})
+  serviceWorker: state.serviceWorker,
+  state
+});
 
 class WorkerProfile extends React.Component {
-    componentDidMount() {
-        this.getCustomers();
-      }
-    
-    getCustomers = () => {
-        let id = this.props.match.params.id
-        this.props.getWorkerProfile(id);
-      };
 
+  state = { tip: "" };
+  
+  componentDidMount() {
+    this.getCustomers();
+  }
+
+  getCustomers = () => {
+    let id = this.props.match.params.id;
+    this.props.getWorkerProfile(id);
+  };
 
   submitTip = tip => {
     this.props.sendTip(this.props.match.params.id, tip).then(res => {
@@ -28,29 +30,28 @@ class WorkerProfile extends React.Component {
     });
   };
 
-    deleteServiceWorkers = () => {
-        let id = this.props.match.params.id
-          this.props.deleteServiceWorkers(id);     
-    }
-    
-    render() {
-      let { tip } = this.state;
-      return (
-        <div className="ui centered card" style={{ marginTop: "50px" }}>
-          {" "}
-          <WorkerList
-            {...this.props.serviceWorker}
-            single={true}
-            submitTip={this.submitTip}
-          />
-          <Modal open={this.state.modal}>
-            <SuccessModal {...this.props.serviceWorker} tipAmount={tip} />
-          </Modal>
-        </div>
-      );
-   }
+  deleteServiceWorkers = () => {
+    let id = this.props.match.params.id;
+    this.props.deleteServiceWorkers(id);
+  };
+
+  render() {
+    let { tip } = this.state;
+    return (
+      <div className="ui centered card" style={{ marginTop: "50px" }}>
+        {" "}
+        <WorkerList
+          {...this.props.serviceWorker}
+          single={true}
+          submitTip={this.submitTip}
+        />
+        <Modal open={this.state.modal}>
+          <SuccessModal {...this.props.serviceWorker} tipAmount={tip} />
+        </Modal>
+      </div>
+    );
+  }
 }
-  
 
 export default connect(
   mapStateToProps,
